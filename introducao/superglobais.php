7e2206e44d5eae811ae00a29e0947b53a7ebb565
J-Breno <joao.breno85@hotmail.com>
2024-06-20 12:53:44 -0300

    superglobais

---
+++ introducao/superglobais.php
@@ -0,0 +1,12 @@
+<?php
+echo '<pre>';
+var_dump($_SERVER);
+echo '</pre>';
+
+echo '<pre>';
+var_dump($_GET); // elementos das variáveis enviadas na URL
+echo '</pre>';
+
+echo '<pre>';
+var_dump($_POST); // enviadas por um formulario
+echo '</pre>';
