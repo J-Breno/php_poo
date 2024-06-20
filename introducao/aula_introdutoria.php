b1a0b74cc9c94fd6e9fb9eb777aae5a43149c938
J-Breno <joao.breno85@hotmail.com>
2024-06-20 12:31:53 -0300

    Introdução ao php, estudando variáveis e alguns metodos

---
+++ introducao/aula_introdutoria.php
@@ -0,0 +1,17 @@
+<?php
+
+echo 'teste';
+
+// isso é um comentário online
+/*
+ * Isso é um comentário blockline
+ * */
+
+$a = 'Teste';
+echo $a, 'do php';
+print $a;
+//use eles para debugar o seu código
+var_dump($a); // ele mostra o tipo dela, o tamanho dela, e o seu conteudo
+print_r($a); //versão resumida do var_dump
+
+
---
+++ introducao/variaveis.php
@@ -0,0 +1,12 @@
+<?php
+$nome = 'João';
+$sobrenome = 'Breno';
+echo 'Meu nome é: '. $nome . ' ' . $sobrenome;
+
+$nome_completo = $nome . ' ' . $sobrenome;
+$nome_completo_ = "{$nome} {$sobrenome}"; // para
+$x = 10;
+$y = 5;
+$multiplicacao = $x * $y;
+
+var_dump($multiplicacao);
